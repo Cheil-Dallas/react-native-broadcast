0c965bd4499bc6170a4d7024875e788721a2df2c
Sen Doan <sendoan@Sen-Doan-GGHFXYD37M-Mac-Kandji-Level-IV-2025.local>
2025-11-10 18:17:29 -0600

    add MainApplicatoin

---
+++ android/src/main/java/com/cheil/broadcast/MainApplication.kt
@@ -0,0 +1,38 @@
+package com.yourappname
+
+import android.app.Application
+import com.facebook.react.PackageList
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
+import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.soloader.SoLoader
+import com.yourpackage.broadcast.BroadcastPackage  // Add this import
+
+class MainApplication : Application(), ReactApplication {
+
+  private val mReactNativeHost: ReactNativeHost =
+    object : DefaultReactNativeHost(this) {
+      override fun getPackages(): List<ReactPackage> {
+        val packages = PackageList(this).packages
+
+        // Add this line manually if autolinking didn’t work
+        packages.add(BroadcastPackage())
+
+        return packages
+      }
+
+      override fun getJSMainModuleName(): String = "index"
+    }
+
+  override fun getReactNativeHost(): ReactNativeHost = mReactNativeHost
+
+  override fun onCreate() {
+    super.onCreate()
+    SoLoader.init(this, false)
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      DefaultNewArchitectureEntryPoint.load()
+    }
+  }
+}
